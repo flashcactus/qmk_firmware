9aa3acc9e364fd5a17081abaa14f685906975e0f
Ivan Uvarov <flashcactus@ya.ru>
2018-05-17 15:36:51 +0300

    Fix comments&readme; change KC_MENU key to KC_APP

--- keyboards/satan/keymaps/cactus/README.md
+++ keyboards/satan/keymaps/cactus/README.md
@@ -1,20 +1,21 @@
-Unxmaal's GH60 Satan Layout
+prefixcactus' GH60 Satan Layout
 =====================
-* Mostly stolen from /u/robotmaxtron
+* Based on Unxmaal's layout from the QMK repo
 
 ##Quantum MK Firmware
 For the full Quantum feature list, see the parent readme.md.
 
-* Standard Mac ANSI layout
+* Standard ANSI layout except for sun backspace
 * Spacebar acts as space when tapped, Fn when held
 * Menu acts as menu when tapped, Fn2 when held
 * Layer1:
-  * Top row = `~, F1-F12, Del
-  * JKIL = arrow cluster
-* Layer2:
-  * Top row = media controls
-  * JKIL = PgDn/Up/Home/Insert
-  * Backspace = Reset
+  * Top row = `~, F1-F12, Ins/Del
+  * JKIL = arrow cluster; UO = Home/End; P: and HN are Pgup/Pgdn
+  * Media buttons under arrow cluster
+  * Left side is for volume and screen backlight buttons plus QMK Lock key
+* Layer2: Mouse controls on the left, keyboard backlight under that (see keymap file) 
+
+See layout.c for the full map
 
 ### Additional Credits
 Keymap has been based on various keymaps available from the QMK Repo for the GH60-SATAN and KC60 keyboards.
--- keyboards/satan/keymaps/cactus/keymap.c
+++ keyboards/satan/keymaps/cactus/keymap.c
@@ -16,17 +16,17 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    /*
-   *  ANSI Base, Mac style
+   *  ANSI Base
    * ,-----------------------------------------------------------------------------.
    * |Esc   |  1|   2|   3|   4|   5|   6|   7|   8|   9|   0|   -|   = |    `|   \|
    * |-----------------------------------------------------------------------------|
    * |Tab    |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I|   O|  P|  [|  ]|  Bksp|
    * |-----------------------------------------------------------------------------|
-   * |Caps/Fn      |    A|    S|    D|    F|   G|  H|  J|  K|  L|  ;|  '|   Enter  |
+   * |Caps         |    A|    S|    D|    F|   G|  H|  J|  K|  L|  ;|  '|   Enter  |
    * |-----------------------------------------------------------------------------|
    * |Shift      |    Z|     X|    C|     V|  B|  N|  M|  ,|  .|  /|     Shift |   |
    * |-----------------------------------------------------------------------------|
-   * |Fn|Alt |Gui | Space(tapped), Fn(held) |Gui |Alt  |Menu(tapped, Fn2(held)|Ctrl|
+   * |Ctrl|Gui |Alt| Space(tapped), Fn(held) |Alt |Gui |Menu(tapped, Fn2(held)|Ctrl|
    * `-----------------------------------------------------------------------------'
    */
 [ _base ] = KEYMAP_HHKB(
@@ -34,9 +34,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,   KC_W,  KC_E,    KC_R,  KC_T,   KC_Y,    KC_U,   KC_I,  KC_O,    KC_P,    KC_LBRC,  KC_RBRC,  KC_BSPC, \
   KC_CAPS, KC_A,   KC_S,  KC_D,    KC_F,  KC_G,   KC_H,    KC_J,   KC_K,  KC_L,    KC_SCLN, KC_QUOT,            KC_ENT,  \
   KC_LSFT,         KC_Z,  KC_X,    KC_C,  KC_V,   KC_B,    KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH,            KC_RSFT, KC_NO  , \
-  KC_LCTL, KC_LGUI,KC_LALT,        LT(_space,KC_SPACE),                      KC_RALT, KC_RGUI, LT(_menu,KC_MENU),    KC_RCTL),
+  KC_LCTL, KC_LGUI,KC_LALT,        LT(_space,KC_SPACE),                      KC_RALT, KC_RGUI, LT(_menu,KC_APP),    KC_RCTL),
    /*
-   * Pok3r style arrow cluster
+   * arrow cluster under right hand on home row
    * ,------------------------------------------------------------.
    * |`~ | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|DEL|INS |
    * |------------------------------------------------------------|
@@ -53,9 +53,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, KC_DELETE,   KC_INSERT,\
   KC_TRNS,  KC_TRNS, KC_VOLU, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_UP  , KC_END , KC_PGUP, KC_TRNS, KC_TRNS, KC_DEL,  \
   KC_TRNS,  KC_TRNS, KC_VOLD, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_PSCR, KC_TRNS,  \
-  KC_TRNS,  KC_LOCK, KC_MUTE, KC_TRNS, KC_TRNS, KC_SPACE,KC_PGDN, KC_MPLY, KC_MPRV, KC_MNXT, KC_TRNS, KC_TRNS, KC_TRNS, \
-  KC_TRNS,  KC_TRNS, KC_TRNS,                   KC_TRNS,                           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS), 
-  /* Keymap _FL: Function Layer
+  KC_TRNS,  KC_LOCK, KC_MUTE, KC_TRNS, KC_LOCK, KC_SPACE,KC_PGDN, KC_MPLY, KC_MPRV, KC_MNXT, KC_TRNS, KC_TRNS, KC_TRNS, \
+  KC_TRNS,  KC_TRNS, KC_TRNS,                   KC_TRNS,                           KC_TRNS, KC_TRNS, KC_MENU, KC_TRNS), 
+  /* Backlight and mouse
    * ,-----------------------------------------------------------.
    * |   |   |   |   |   |   |   |   |   |   |   |   |   |  RESET|
    * |-----------------------------------------------------------|
